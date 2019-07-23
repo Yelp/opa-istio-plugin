ba25ebfb66dec7cad0b0c7bc384c3d1db5e855d9
Daniel Popescu <dpopes@yelp.com>
2019-07-23 16:54:32 -0700

    Configure the plugin to log in json format

--- internal/internal.go
+++ internal/internal.go
@@ -50,6 +50,9 @@ type evalResult struct {
 // instantiate the plugin.
 func Validate(m *plugins.Manager, bs []byte) (*Config, error) {
 
+	// Default to logging consistently with the default in Opa server
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
 	cfg := Config{
 		Addr:   defaultAddr,
 		Query:  defaultQuery,
